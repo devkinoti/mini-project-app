66d04078d6d9af3297cc1ecc7d03665065bc6992
Chris Kinoti <devkinoti@gmail.com>
2022-10-31 11:25:07 +0300

    pages theme display

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :projects
   root to: 'pages#home'
   get 'dashboard/index'
+  get 'pricing', to: 'pages#pricing', as: :pricing
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
