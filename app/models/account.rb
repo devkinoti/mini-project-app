4ac20b2f59cf2563f4c8fc14a56c0f0c51bd943d
Chris Kinoti <devkinoti@gmail.com>
2022-10-31 13:41:10 +0300

    setting up user as owner with one account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id         :uuid             not null, primary key
+#  name       :string           not null
+#  user_id    :uuid             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  sluq       :string
+#
 class Account < ApplicationRecord
+  extend FriendlyId
+
+  friendly_id :name, use: :slugged
   belongs_to :user
+
+  # update friendly_id when slug is changed
+  def should_generate_new_friendly_id?
+    name_changed?
+  end
 end
--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id         :uuid             not null, primary key
+#  name       :string           not null
+#  user_id    :uuid             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  sluq       :string
+#
 require "test_helper"
 
 class AccountTest < ActiveSupport::TestCase
