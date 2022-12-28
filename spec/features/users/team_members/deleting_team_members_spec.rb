6b39e11bc88459db21c09d9bcf743bdf9ab7cf01
Chris Kinoti <devkinoti@gmail.com>
2022-12-29 02:40:28 +0300

    Editing and deleting team members specs

---
+++ spec/features/users/team_members/deleting_team_members_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.feature "Users can delete team members" do 
+  let(:user) do 
+    FactoryBot.create(:user)
+  end
+
+  let(:team_member) do 
+    FactoryBot.create(:team_member, account: user.account)
+  end
+
+  before do 
+    login_as(user)
+
+    visit team_member_path(team_member)
+  end
+
+  scenario "successfully" do 
+    click_link "Destroy"
+
+    expect(page).to have_content("Team Member has been deleted")
+    expect(page.current_url).to eq(team_members_url)
+  end
+
+
+
+end
