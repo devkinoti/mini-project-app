5d47e7a8bccb4bd7fb8d57b5a5da9d4c23cc521f
Chris Kinoti <devkinoti@gmail.com>
2022-12-28 21:34:54 +0300

    signing in for account owner feature

---
+++ spec/features/users/signing_in_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'Users can sign in' do
+  let(:user) do
+    FactoryBot.create(:user)
+  end
+
+  before do
+    user.confirm
+  end
+
+  scenario 'with valid credentials' do
+    visit '/'
+
+    click_link 'Sign in as account owner'
+
+    fill_in 'Email address', with: user.email
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+
+    expect(page).to have_content "Signed in successfully"
+    expect(page).to have_content "Hello, #{user.first_name}"
+  end
+end
